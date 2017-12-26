33a08ed8f63eb0f929b558d4af9fb52f58c18627
Ben Stones <ben@b3ns.com>
2017-12-26 21:40:44 +0000

    Implement logic to retrieve exchange rates from the supported provider, and return the converted amount

--- app/Support/FloatRatesCurrencyConversion.php
+++ app/Support/FloatRatesCurrencyConversion.php
@@ -4,7 +4,10 @@ namespace App\Support;
 
 use App\Contracts\CurrencyConversion;
 use App\Traits\CurrencyCodes;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use InvalidArgumentException;
+use RuntimeException;
 
 class FloatRatesCurrencyConversion implements CurrencyConversion
 {
@@ -26,6 +29,11 @@ class FloatRatesCurrencyConversion implements CurrencyConversion
     protected $with;
 
     /**
+     * @var string
+     */
+    protected $source = 'https://www.floatrates.com/daily/';
+
+    /**
      * @param string $code
      * @return $this
      * @throws InvalidArgumentException
@@ -33,6 +41,8 @@ class FloatRatesCurrencyConversion implements CurrencyConversion
     public function from(string $code)
     {
         $this->validateOrFail($code);
+
+        $this->from = $code;
 
         return $this;
     }
@@ -46,15 +56,19 @@ class FloatRatesCurrencyConversion implements CurrencyConversion
     {
         $this->validateOrFail($code);
 
+        $this->to = $code;
+
         return $this;
     }
 
     /**
-     * @param int $amount
+     * @param float $amount
      * @return int
      */
-    public function with(int $amount)
+    public function with(float $amount)
     {
+        $this->with = $amount;
+
         return $this->convert();
     }
 
@@ -65,9 +79,59 @@ class FloatRatesCurrencyConversion implements CurrencyConversion
     protected function convert()
     {
         if (empty($this->from) || empty($this->to)) {
-            throw new InvalidArgumentException('Cannot convert without two currencies provided.');
+            throw new InvalidArgumentException('Cannot convert without two valid currencies.');
         }
 
-        return 0;
+        $rate = $this->getExchangeRate();
+
+        return $this->with * $rate;
+    }
+
+    /**
+     * @return int
+     */
+    protected function getExchangeRate()
+    {
+        if (cache()->has($this->from)) {
+            $to = strtolower($this->to);
+
+            return cache($this->from)->{$to}->rate;
+        } else {
+            // Source URL must have a trailing slash
+            $source = (substr($this->source, -1) !== '/') ? $this->source . '/' : $this->source;
+
+            // Generate an HTTP request to retrieve the daily exchange rate
+            $client = new Client([
+                'base_uri' => $source,
+                'http_errors' => true,
+            ]);
+
+            try {
+                // Retrieve the exchange rate with the currency we are converting from
+                $response = $client->request('GET', $this->from . '.json');
+
+                // Decode the returned JSON string and return an exception if it fails
+                $rates = json_decode($response->getBody());
+
+                if ($rates === null) {
+                    throw new RuntimeException('Conversion failed: cannot parse JSON string');
+                }
+
+                // Cache the exchange rate
+                cache([$this->from => $rates], now()->addDay());
+
+                $to = strtolower($this->to);
+
+                return $rates->{$to}->rate;
+            } catch (RequestException $e) {
+
+                throw new RuntimeException('Conversion failed: DNS or timeout error. Status code: ' . $e->getCode());
+
+            } catch (RuntimeException $e) {
+
+                throw new RuntimeException('Conversion failed. Status code: ' . $e->getCode());
+
+            }
+        }
     }
 }
