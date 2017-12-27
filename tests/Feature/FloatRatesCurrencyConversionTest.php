d4e0480d20b16487613dd806a8dfe919fcd61d17
Ben Stones <ben@b3ns.com>
2017-12-27 23:23:38 +0000

    Add feature test for FloatRatesCurrencyConversion class

---
+++ tests/Feature/FloatRatesCurrencyConversionTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Support\FloatRatesCurrencyConversion;
+use App\Traits\CurrencyCodes;
+use Tests\TestCase;
+
+class FloatRatesCurrencyConversionTest extends TestCase
+{
+    use CurrencyCodes;
+
+    /**
+     * @var FloatRatesCurrencyConversion
+     */
+    protected $conversion;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->conversion = new FloatRatesCurrencyConversion();
+    }
+
+    /**
+     * @return void
+     */
+    public function test_from_method_validates_each_currency_code_and_returns_instance_of_self()
+    {
+        foreach ($this->currencies as $currency) {
+            $instance = $this->conversion->to($currency);
+
+            self::assertInstanceOf(FloatRatesCurrencyConversion::class, $instance);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function test_to_method_validates_each_currency_code_and_returns_instance_of_self()
+    {
+        foreach ($this->currencies as $currency) {
+            $instance = $this->conversion->from($currency);
+
+            self::assertInstanceOf(FloatRatesCurrencyConversion::class, $instance);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function test_with_method_returns_array_of_converted_currencies_when_currencies_identical()
+    {
+        $currency = $this->currencies[0];
+        $conversion = $this->conversion->from($currency)->to($currency)->with(1);
+
+        self::assertInternalType('array', $conversion);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_with_method_returns_array_of_converted_currencies_when_currencies_differ()
+    {
+        $from = $this->currencies[0];
+        $to = $this->currencies[1];
+        $conversion = $this->conversion->from($from)->to($to)->with(1);
+
+        self::assertInternalType('array', $conversion);
+    }
+}
