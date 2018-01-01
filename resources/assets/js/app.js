bc79c5e6bcdf8f351f59188748ab971bcbccd0d3
Ben Stones <ben@b3ns.com>
2018-01-01 23:31:21 +0000

    1/ Include CSRF token in meta
    2/ Use Vue/Axios to handle API request to retrieve and display currency information

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,3 +2,47 @@
  * Import the dependencies
  */
 require('./bootstrap');
+
+new Vue({
+    el: '#root',
+
+    data: {
+        success: {
+            display: false,
+
+            description: '',
+        },
+
+        error: {
+            display: false,
+
+            description: 'An error occurred. Please try again.',
+        },
+
+        from: 'GBP',
+        to: 'USD',
+        amount: '',
+    },
+
+    methods: {
+        calculate() {
+            axios.post('api/convert', {
+                from: this.from,
+                to: this.to,
+                amount: this.amount,
+            }, {
+                headers: {
+                    accept: 'application/json'
+                }
+            }).then((response) => {
+                this.error.display = false;
+
+                this.success.description = response.data.result.description;
+                this.success.display = true;
+            }).catch((error) => {
+                this.success.display = false;
+                this.error.display = true;
+            });
+        }
+    }
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,30 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <title>Currency Convertor</title>
 </head>
 <body>
-    <div class="container">
-        <form>
+    <div id="root" class="container">
+        <div class="mt-4 p-3 bg-dark rounded text-white d-none" :class="{ 'd-block': success.display }">
+            @{{ success.description }}
+        </div>
+
+        <div class="alert alert-danger mt-4 p-3 rounded d-none" :class="{ 'd-block': error.display }">
+            @{{ error.description }}
+        </div>
+
+        <form @submit.prevent="calculate">
             <div class="row pt-4 pb-4">
                 <div class="col-lg-3 pt-2 pb-2">
                     <div class="input-group">
                         <span class="input-group-addon oi" data-glyph="info"></span>
 
-                        <input class="form-control input-lg" type="text" name="amount" placeholder="Enter a value...">
+                        <input class="form-control input-lg" type="text" name="amount" placeholder="Enter a value..."
+                               v-model="amount" required>
                     </div>
                 </div>
 
@@ -25,10 +35,10 @@
                     <div class="input-group">
                         <span class="input-group-addon oi" data-glyph="flag"></span>
 
-                        <select class="form-control" name="first">
-                            <option selected>US Dollar (USD)</option>
-                            <option>British Pound (GBP)</option>
-                            <option>Euro (EUR)</option>
+                        <select class="form-control" name="first" v-model="from">
+                            <option value="USD" selected>US Dollar (USD)</option>
+                            <option value="GBP">British Pound (GBP)</option>
+                            <option value="EUR">Euro (EUR)</option>
                         </select>
                     </div>
                 </div>
@@ -37,10 +47,10 @@
                     <div class="input-group">
                         <span class="input-group-addon oi" data-glyph="flag"></span>
 
-                        <select class="form-control" name="second">
-                            <option>US Dollar (USD)</option>
-                            <option selected>British Pound (GBP)</option>
-                            <option>Euro (EUR)</option>
+                        <select class="form-control" name="second" v-model="to">
+                            <option value="USD" selected>US Dollar (USD)</option>
+                            <option value="GBP">British Pound (GBP)</option>
+                            <option value="EUR">Euro (EUR)</option>
                         </select>
                     </div>
                 </div>
@@ -53,5 +63,7 @@
             </div>
         </form>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
