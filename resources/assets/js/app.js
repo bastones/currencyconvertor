5450936594d686a478462a94182b9b2c0d9b8d7b
Ben Stones <ben@b3ns.com>
2018-01-02 15:55:02 +0000

    Display a friendly notice on the button when conversation data, and disable the button until the data is returned

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,8 @@ new Vue({
             description: 'An error occurred. Please try again.',
         },
 
+        wait: false,
+
         from: Object.keys(currencies)[0],
         to: Object.keys(currencies)[1],
         amount: '',
@@ -24,6 +26,8 @@ new Vue({
 
     methods: {
         calculate() {
+            this.wait = true;
+
             axios.post('api/convert', {
                 from: this.from,
                 to: this.to,
@@ -33,11 +37,15 @@ new Vue({
                     accept: 'application/json'
                 }
             }).then((response) => {
+                this.wait = false;
+
                 this.error.display = false;
 
                 this.success.description = response.data.result.description;
                 this.success.display = true;
             }).catch((error) => {
+                this.wait = false;
+
                 this.success.display = false;
                 this.error.display = true;
 
